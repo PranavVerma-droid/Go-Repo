dd15dd11c7d757fdc1927dddecedcc04cef6190e
Pranav Verma <pranav@verma.net.in>
2023-02-21 21:18:38 +0530

    Update

---
+++ CodeCademy/Basics/Ascii-Art.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("      `.-::::::-.`")
+  fmt.Println("  .:-::::::::::::::-:.")
+  fmt.Println("  `_:::    ::    :::_`")
+  fmt.Println("   .:( ^   :: ^   ):.")
+  fmt.Println("   `:::   (..)   :::.")
+  fmt.Println("   `:::::::UU:::::::`")
+  fmt.Println("   .::::::::::::::::.")
+  fmt.Println("   O::::::::::::::::O")
+  fmt.Println("   -::::::::::::::::-")
+  fmt.Println("   `::::::::::::::::`")
+  fmt.Println("    .::::::::::::::.")
+  fmt.Println("      oO:::::::Oo")
+  
+  fmt.Println("")
+  fmt.Println("")
+  fmt.Println("")
+
+  fmt.Println("     z$$$$$. $$")
+  fmt.Println("    $$$$$$$$$$$")
+  fmt.Println("  $$$$$$**$$$$             eeeeer")
+  fmt.Println("  $$$$$%   '$$$             $$$$$F")
+  fmt.Println(" 4$$$$P     *$$             *$$$$F")
+  fmt.Println(" $$$$$      '$$    .ee.      ^$$$F            ..e.")
+  fmt.Println(" $$$$$       \"\"  .$$$$$$b     $$$F 4$$$$$$   $$$$$$c")
+  fmt.Println("4$$$$F          4$$$\"\"$$$$    $$$F '*$$$$*  $$$P$$$L")
+  fmt.Println("4$$$$F         .$$$F  ^$$$b   $$$F  J$$$   $$$$  ^$$$.")
+  fmt.Println("4$$$$F         d$$$    $$$$   $$$F J$$P   .$$$F   $$$$")
+  fmt.Println("4$$$$F         $$$$    3$$$F  $$$FJ$$P    4$$$   $$$$")
+  fmt.Println("4$$$$F        4$$$$    4$$$$  $$$$$$$r    $$$$$$$$$$$$")
+  fmt.Println("4$$$$$        4$$$$    4$$$$  $$$$$$$$    $$$$********")
+  fmt.Println(" $$$$$        4$$$$    4$$$F  $$$F4$$$b   *$$$r")
+  fmt.Println(" 3$$$$F       d$$$$    $$$$\"  $$$F *$$$F  4$$$L")
+  fmt.Println("  $$$$$.     d$$$$$.   $$$$   $$$F  $$$$.  $$$$    z$P")
+  fmt.Println("   $$$$$e..d$$$\"$$$b  4$$$\"  J$$$L  '$$$$  '$$$b..d$$")
+  fmt.Println("    *$$$$$$$$$  ^$$$be$$$\"  $$$$$$$  3$$$$F \"$$$$$$$")
+  fmt.Println("     ^*$$$$P\"     *$$$$*    $$$$$$$   $$$$F  ^*$$$\"")
+
+
+}
