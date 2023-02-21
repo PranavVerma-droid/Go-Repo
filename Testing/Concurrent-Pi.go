1670643ccb29f0b78c27cd217126f71a0bf4d58c
Pranav Verma <pranav@verma.net.in>
2023-02-21 21:49:26 +0530

    Update

---
+++ Testing/Concurrent-Pi.go
@@ -0,0 +1,33 @@
+//This File was made my Pranav Verma.
+// https://github.com/PranavVerma-droid
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("          math.Pi:", math.Pi)
+	fmt.Println("Nilakantha Series:", pi(5000))
+}
+
+func pi(n int) float64 {
+	ch := make(chan float64)
+	for k := 0; k < n; k++ {
+		go term(ch, float64(k))
+	}
+	f := 3.0
+	for k := 0; k < n; k++ {
+		f += <-ch
+	}
+	return f
+}
+
+func term(ch chan float64, k float64) {
+	ch <- 4 * math.Pow(-1, k) / ((2*k + 2) * (2*k + 3) * (2*k + 4))
+}
+
+//This is pretty simple code ngl
+//Please follow, this took a long time to write :/
